590e899835d444478901208fc343edf25cb4bab1
dumblearner <bishal@gonitsora.com>
2026-06-16 21:49:16 +0800

    TP page added. Eulerian triangle conjecture

---
+++ themes/mathjax-preamble.js
@@ -0,0 +1,17 @@
+// mathjax-preamble.js
+window.MathJax = {
+    tex: {
+        macros: {
+            // Eulerian numbers
+            euler: ['{\\genfrac{\\langle}{\\rangle}{0pt}{0}{#1}{#2}}', 2],
+            
+            // Clean Eulerian numbers (with "clean" superscript)
+            cleaneuler: ['{\\genfrac{\\langle}{\\rangle}{0pt}{0}{#1}{#2}^{\\text{clean}}}', 2]
+            
+        }
+    },
+    // Optional: Add other MathJax settings here
+    options: {
+        enableMenu: false  // Disable the right-click menu if you prefer
+    }
+};
