020b304f3ef35a179fad3a90721a8fe79fedd00d
dumblearner <bishal@gonitsora.com>
2026-07-05 02:40:57 +0800

    Eulerian square

--- themes/mathjax-preamble.js
+++ themes/mathjax-preamble.js
@@ -8,6 +8,8 @@ window.MathJax = {
             // Clean Eulerian numbers (with "clean" superscript)
             cleaneuler: ['{\\genfrac{\\langle}{\\rangle}{0pt}{0}{#1}{#2}^{\\text{clean}}}', 2]
             
+
+            
         }
     },
     // Optional: Add other MathJax settings here
